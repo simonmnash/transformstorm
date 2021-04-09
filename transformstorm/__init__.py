529332f0ede79cecd6717a9707df032497b7822d
Simon <simon@pop-os.localdomain>
2021-04-09 17:25:47 -0400

    gets rid of a bunch of random repeated blocks of curses draws. leans more into a couple big global objects, which probably isn't super sustainable but is keeping things organized for now. still a bunch of quirks in random places, some will be fixed, some will remain, and some will be made quirkier.

--- transformstorm/__init__.py
+++ transformstorm/__init__.py
@@ -1,150 +1,80 @@
-import sys,os
-import curses
-from textgenerator import TextGenerator
-import logging.config
-import textwrap
-import threading
+import sys, os, curses, logging.config, threading
 from time import sleep
+from interfaces import TextAccumulator, OptionWindow
 
 from random import randint
 logging.config.dictConfig({
     'version': 1,
     'disable_existing_loggers': True,
 })
-text_boxes = {}
+
 highlighted_slot_index = 0
-slots = [0, 2, 4, 6, 8, 10]
-class TextAccumulator():
-    def __init__(self):
-        self.complete_text = ""
-        self.text_generator = TextGenerator() 
-    def add_text(self, text):
-        self.complete_text = self.complete_text + text
-    def generate_add_text_candidate(self):
-        return self.text_generator.create_textblock(self.complete_text)
+option_window = OptionWindow()
 
-class TextBox():
-    def __init__(self, slot, text, highlighted = False):
-        self.text = text
-        self.slot = slot
-        self.highlighted = highlighted
-        self.box = self.draw()
-    
-    def draw(self):
-        new_box = curses.newwin(2, 75, self.slot, 55)
-        if self.highlighted:
-            new_box.attron(curses.color_pair(3))
-            new_box.attron(curses.A_BOLD)
-        new_box.addstr(1, 2, textwrap.fill(self.text, 75))
-        if self.highlighted:
-            new_box.attroff(curses.color_pair(3))
-            new_box.attroff(curses.A_BOLD)
-        return new_box
-
-    def toggle_highlight(self):
-        if self.highlighted:
-            self.highlighted = False
-        else:
-            self.highlighted = True
-        self.box = self.draw()
-        self.box.refresh()
-
-    def refresh(self):
-        self.box.refresh()
 
 global_text_accumulator = TextAccumulator()
 
 def input_thread(stdscr):
     global current_slot_index
-    global highlighted_slot_index
-    global slots
+    global option_window
     curses.curs_set(0)
     k = 0
+    
     while (k != ord('q')):
+        highlighted_slot_index = option_window.highlight_ticker
         if k == curses.KEY_DOWN:
-            if text_boxes.get(highlighted_slot_index):
-                keep_text = text_boxes.get(highlighted_slot_index).text
-                new_box = TextBox(slots[highlighted_slot_index], keep_text, False)
-                text_boxes[highlighted_slot_index]=new_box
-            highlighted_slot_index = (highlighted_slot_index + 1) % len(slots)
-            if text_boxes.get(highlighted_slot_index):
-                keep_text = text_boxes.get(highlighted_slot_index).text
-                new_box = TextBox(slots[highlighted_slot_index], keep_text, True)
-                text_boxes[highlighted_slot_index]=new_box
+            option_window.remove_focus()
+            highlighted_slot_index = (highlighted_slot_index + 2) % 16
+            option_window.focus_option_at_key(highlighted_slot_index)
         if k == curses.KEY_UP:
-            if text_boxes.get(highlighted_slot_index):
-                keep_text = text_boxes.get(highlighted_slot_index).text
-                new_box = TextBox(slots[highlighted_slot_index], keep_text, False)
-                text_boxes[highlighted_slot_index]=new_box
-            highlighted_slot_index = (highlighted_slot_index - 1) % len(slots)
-            if text_boxes.get(highlighted_slot_index):
-                keep_text = text_boxes.get(highlighted_slot_index).text
-                new_box = TextBox(slots[highlighted_slot_index], keep_text, True)
-                text_boxes[highlighted_slot_index]=new_box
-
-        if k == ord(' '):
-            current_text = text_boxes.get(highlighted_slot_index).text
-            global_text_accumulator.add_text(current_text)
-            stdscr.attron(curses.color_pair(2))
-            stdscr.attron(curses.A_BOLD)
-            stdscr.addstr(0, 0, textwrap.fill(global_text_accumulator.complete_text, 50))
-            stdscr.attroff(curses.color_pair(2))
-            stdscr.attroff(curses.A_BOLD)
-            for slot_index in range(0, len(slots)):
-                if slot_index==0:
-                    new_box = TextBox(slots[slot_index], global_text_accumulator.generate_add_text_candidate(), True)
-                else:
-                    new_box = TextBox(slots[slot_index], global_text_accumulator.generate_add_text_candidate(), False)
-                text_boxes[slot_index]=new_box
-            current_slot_index = 0
-            highlighted_slot_index = 0
-        for box in text_boxes.values():
+            option_window.remove_focus()
+            highlighted_slot_index = (highlighted_slot_index - 2) % 16
+            option_window.focus_option_at_key(highlighted_slot_index)
+        if k == curses.KEY_RIGHT:
+            if option_window.options.get(highlighted_slot_index).text != "LOADING...":
+                current_text = option_window.options.get(highlighted_slot_index).text
+                global_text_accumulator.add_text(current_text)
+                option_window.set_screen(stdscr)
+                current_slot_index = 0
+                option_window.highlight_ticker = 0
+    
+        for box in option_window.options.values():
             box.refresh()
         stdscr.refresh()
-
         # Wait for next input
         k = stdscr.getch()
 
 def draw_menu(stdscrr):
     stdscr = curses.initscr()
-
     height, width = stdscr.getmaxyx()
-
     # Start colors in curses
     stdscr.refresh()
     curses.start_color()
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    current_slot_index = 0
-    
-    for slot_index in range(0, len(slots)):
-        if slot_index==0:
-            new_box = TextBox(slots[slot_index], global_text_accumulator.generate_add_text_candidate(), True)
-        else:
-            new_box = TextBox(slots[slot_index], global_text_accumulator.generate_add_text_candidate(), False)
-        text_boxes[slot_index] = new_box
-    
+    option_window.set_screen(stdscr)
+    global_text_accumulator.set_screen(stdscr)
+
     t = threading.Thread(name ='daemon', target=input_thread, args=(stdscr,))
     t.setDaemon(True)
     t.start()
 
     while True:
-        if highlighted_slot_index == current_slot_index:
-            if text_boxes.get(current_slot_index):
-                keep_text = text_boxes.get(current_slot_index).text
-                new_box = TextBox(slots[current_slot_index], keep_text, True)
-            else:
-                new_box = TextBox(slots[current_slot_index], global_text_accumulator.generate_add_text_candidate(), True)
-        else:
-            new_box = TextBox(slots[current_slot_index], global_text_accumulator.generate_add_text_candidate(), False)
-        text_boxes[current_slot_index]=new_box
-        current_slot_index = (current_slot_index + 1) % len(slots)
-        sleep(1.5)
+        option_window.attempt_to_generate_new_option_at_text_ticker_location(global_text_accumulator.generate_add_text_candidate())
+        sleep(0.5)
+
+        option_window.focus_option_at_key(option_window.highlight_ticker)
+        for box in option_window.options.values():
+            box.refresh()
+        stdscr.refresh()
 
 
 def main():
     curses.wrapper(draw_menu)
 
 if __name__ == "__main__":
-    main()
+    if os.path.isdir("model/"):
+        main()
+    else:
+        print("This library doesn't manage models yet, it can only be run in a directory that already has a model subdirectory in it. You should be able to load any huggingface pytorch gpt2 model, or find the one I fined tuned at gyre.itch.io.")
---
+++ transformstorm/interfaces.py
@@ -0,0 +1,99 @@
+import curses, os, textwrap
+from textgenerator import TextGenerator
+
+
+
+class TextAccumulator():
+    def __init__(self):
+        self.complete_text = ""
+        if os.path.isdir("model/"):
+            self.text_generator = TextGenerator()
+        
+    def set_screen(self, stdscr):
+        self.screen = stdscr
+
+    def add_text(self, text):
+        self.complete_text = self.complete_text + text
+        new_box = curses.newwin(100, 60, 0, 75)
+        new_box.attron(curses.color_pair(2))
+        new_box.attron(curses.A_BOLD)
+        new_box.addstr(0, 0, textwrap.fill(self.complete_text, 100))
+        new_box.attroff(curses.color_pair(2))
+        new_box.attroff(curses.A_BOLD)
+        new_box.refresh()
+    
+    def generate_add_text_candidate(self):
+        return self.text_generator.create_textblock(self.complete_text)
+
+
+class TextBox():
+    def __init__(self, slot, text, highlighted = False):
+        self.text = text
+        self.slot = slot
+        self.highlighted = highlighted
+        self.box = self.draw()
+    
+    def draw(self):
+        new_box = curses.newwin(2, 75, self.slot, 0)
+        if self.highlighted:
+            new_box.attron(curses.color_pair(3))
+            new_box.attron(curses.A_BOLD)
+        new_box.addstr(1, 2, textwrap.fill(self.text, 75))
+        if self.highlighted:
+            new_box.attroff(curses.color_pair(3))
+            new_box.attroff(curses.A_BOLD)
+        return new_box
+
+    def focus(self):
+        self.highlighted = True
+        self.box = self.draw()
+        return self
+    
+    def unfocus(self):
+        self.highlighted = False
+        self.box = self.draw()
+        return self
+    
+    def toggle_highlight(self):
+        if self.highlighted:
+            self.highlighted = False
+        else:
+            self.highlighted = True
+        self.box = self.draw()
+        self.box.refresh()
+
+    def refresh(self):
+        self.box.refresh()
+
+class OptionWindow():
+    def __init__(self):
+        self.options = {}
+        # highlight ticker determines which option is currently highlighted.
+        self.highlight_ticker = 0
+        # text generation ticker determines which option is next in line for replacement.
+        self.text_generation_ticker = 0
+
+    def set_screen(self, stdscr):
+        self.screen = stdscr
+        for slot in range(0, 16, 2):
+            self.add_new_option(slot, "LOADING...", slot==0)
+
+    def add_new_option(self, new_option_location, new_option_text, highlighted):
+        new_text_box = TextBox(new_option_location, new_option_text, highlighted)
+        self.options[new_option_location] = new_text_box
+
+    def attempt_to_generate_new_option_at_text_ticker_location(self, new_option_text):
+        if self.text_generation_ticker == self.highlight_ticker and self.options[self.text_generation_ticker].text != "LOADING...":
+            pass
+        else:
+            self.add_new_option(self.text_generation_ticker, new_option_text, False)
+        self.text_generation_ticker = (self.text_generation_ticker + 2) % 16
+
+    def remove_focus(self):
+        for option in self.options.values():
+            self.options[option.slot] = option.unfocus()
+
+    def focus_option_at_key(self, key_to_focus):
+        if self.options.get(key_to_focus):
+            self.highlight_ticker = key_to_focus
+            self.options[key_to_focus] = self.options[key_to_focus].focus()
