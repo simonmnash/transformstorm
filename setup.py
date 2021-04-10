bf08bb94be30fadecedfe9eb780e4b7cd841cd4e
Simon <simon@pop-os.localdomain>
2021-04-09 20:30:03 -0400

    add license and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='transformstorm',
+      version='0.1',
+      description='A command line interface for playing with language models',
+      url='https://github.com/simonmnash/transformstorm',
+      author='Simon Nash',
+      license='MIT',
+      packages=['transformers', 'torch']
+      )
