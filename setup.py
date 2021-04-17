78aa53ed901ee1653ab835b8d51524817f741b3c
Simon <simon@pop-os.localdomain>
2021-04-17 14:11:35 -0400

    increment version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='transformstorm',
-      version='0.1',
+      version='0.12',
       packages = find_packages(),
       description='A command line interface for playing with language models',
       url='https://github.com/simonmnash/transformstorm',
